10a0be8044729df76b46d7d5bd9aa61577dc64eb
Atharva Keshre <atharvakeshre@gmail.com>
2025-01-23 12:25:08 -0500

    Disabled tests

--- src/test/java/com/atharva/journalapp/JournalApplicationTests.java
+++ src/test/java/com/atharva/journalapp/JournalApplicationTests.java
@@ -1,13 +1,13 @@
-package com.atharva.journalapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JournalApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.atharva.journalapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JournalApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
